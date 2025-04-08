9cea9445ebc71663e201f4e0ea51e8da41d14d26
Elian <elianf321@gmail.com>
2025-04-08 16:48:52 -0300

    Varios

---
+++ 2°Año/1°Semestre/AYED/2025/Practicas/AYED/src/tp2/ejercicio7/ParcialArboles.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2.ejercicio7;
+
+import tp2.ejercicio1.BinaryTree;
+
+/**
+ *
+ * @author elian
+ */
+public class ParcialArboles {
+    private BinaryTree<Integer> arbol;
+    
+    
+}
