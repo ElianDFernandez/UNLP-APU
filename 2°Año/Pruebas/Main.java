19aebd209740469deaacf3d737ef9927283c54d4
Elian Fernandez <108306724+ElianDFernandez@users.noreply.github.com>
2025-08-23 19:46:32 +0000

    Pruebas

---
+++ 2°Año/Pruebas/Main.java
@@ -0,0 +1,11 @@
+// Clase main pruebas
+
+public class Main {
+    public static void main(String[] args) {
+        // Crear instancia de la clase Pruebas
+        Pruebas pruebas = new Pruebas();
+
+        // Llamar al método para ejecutar las pruebas
+        pruebas.ejecutarPruebas();
+    }
+}
